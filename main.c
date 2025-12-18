37524b1322ae7d120ff56008693445dc2fded182
Noemie Grau <nograu@c3r5p7.42perpignan.fr>
2025-12-18 16:47:51 +0100

    adding end of parsing

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nograu <nograu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/13 15:01:24 by nograu            #+#    #+#             */
-/*   Updated: 2025/12/14 20:36:30 by nograu           ###   ########.fr       */
+/*   Updated: 2025/12/18 16:32:27 by nograu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,66 @@
 
 #include <stdio.h> // a supprimer
 
-//fonction doublons
+long ft_atoi_long(const char *nptr)
+{
+	int		i;
+	int		sign;
+	long	result;
 
-//fonction overflow
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (nptr[i] == 32 || (nptr[i] >= 9 && nptr[i] <= 13))
+		i++;
+	if (nptr[i] == '+' || nptr[i] == '-')
+	{
+		if (nptr[i] == '-')
+			sign = -sign;
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		result = result * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
+
+int	is_dup(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			if (ft_atoi(argv[i]) == ft_atoi(argv[j]))
+				return (write(2, "Error\n", 6), 1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	is_over(int argc, char **argv)
+{ 
+	long	n;
+	int		i;
+
+	i = 1;
+	while (i < argc)
+	{
+		n = ft_atoi_long(argv[i]);
+		if (n < INT_MIN || n > INT_MAX)
+			return (write(2, "Error\n", 6), 1);
+		i++;
+	}
+	return (0);
+}
 
 int	is_valid_num(int argc, char **argv)
 {
@@ -28,19 +85,19 @@ int	is_valid_num(int argc, char **argv)
 	while (i < argc)
 	{
 		if (argv[i][0] == '\0' || (ft_strlen(argv[i]) == 1 && (argv[i][0] == '+' || argv[i][0] == '-')))
-			return (write(2, "Error\n", 6), 0);
+			return (write(2, "Error\n", 6), 1);
 		j = 0;
 		if (argv[i][j] == '+' || argv[i][j] == '-')
 			j++;
 		while (argv[i][j])
 		{
 			if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
-				return (write(2, "Error\n", 6), 0);
+				return (write(2, "Error\n", 6), 1);
 			j++;
 		}
 		i++;
 	}
-	return (1);
+	return (0);
 }
 
 int	main(int argc, char **argv)
@@ -54,11 +111,16 @@ int	main(int argc, char **argv)
 	i = 1;
 	if (argc < 2)
 		return (0);
-	if (!is_valid_num(argc, argv))
+	if (is_valid_num(argc, argv)) // if(0) if (isvalnum() != 0) -> if (1)
+		return (1);
+	if (is_dup(argc, argv))
+		return (1);
+	if (is_over(argc, argv))
 		return (1);
 	while (i < argc)
 		ps_lstadd_back(&a, ps_lstnew(ft_atoi(argv[i++])));
-	// passer a l'algo
+	// algo
+	write(1, "Success\n", 8);
 	return (0);
 }
 
